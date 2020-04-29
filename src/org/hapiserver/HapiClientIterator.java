d53ba3bb062ac12f0c11a59473aaf1d77296b87e
jbfaden <faden@cottagesystems.com>
2020-04-29 08:16:44 -0500

    CSV data response can be JSON response with error code.

--- src/org/hapiserver/HapiClientIterator.java
+++ src/org/hapiserver/HapiClientIterator.java
@@ -5,6 +5,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
+import java.util.logging.Logger;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -14,10 +15,30 @@ import org.json.JSONObject;
  */
 public class HapiClientIterator implements Iterator<HapiRecord> {
     
+    private static final Logger logger= Logger.getLogger("org.hapiserver");
+    
     String nextLine;
     JSONObject info;
     BufferedReader reader;
     CSVHapiRecordConverter converter;
+    
+    /**
+     * because JSON has been detected in the first line, read the rest of the
+     * content from the BufferedReader.  Lines are read into builder, and a 
+     * non-JSON line may be returned.  Either way the reader will not be closed.
+     * 
+     * @param reader
+     * @param builder
+     * @return the next line if read, or null.
+     */
+    private static String readJSON( BufferedReader reader, StringBuilder builder ) throws IOException {
+        String line= reader.readLine();
+        while ( line!=null ) {
+            builder.append(line);
+            line= reader.readLine();
+        }
+        return line; // this implementation assumes there is no data following.
+    }
     
     /**
      * create an iterator for the CSV stream.
@@ -30,6 +51,25 @@ public class HapiClientIterator implements Iterator<HapiRecord> {
         this.info= info;
         this.reader = reader;
         this.nextLine = this.reader.readLine();
+        if ( this.nextLine!=null && this.nextLine.startsWith("{") ) {
+            StringBuilder b= new StringBuilder(this.nextLine);
+            this.nextLine= readJSON( reader, b );
+            if ( this.nextLine==null ) {
+                reader.close();
+            }
+            JSONObject o= new JSONObject(b.toString());
+            if ( !o.has("status") ) {
+                throw new IllegalArgumentException("expected to see status in JSON response.");
+            } else {
+                JSONObject status= o.getJSONObject("status");
+                int statusCode= status.getInt("code");
+                if ( statusCode==1201 ) {
+                    logger.fine("do nothing, this.nextLine is already set.");
+                } else {
+                    throw new IOException("error from server: "+statusCode+ " " +status.getString("message") );
+                }
+            }
+        }
         this.converter= new CSVHapiRecordConverter(info);
     }
 
