745393c43e0c0f15da8d95823389928baef0fb06
jbfaden <faden@cottagesystems.com>
2020-05-11 11:09:03 -0500

    javadoc tests

--- src/test/DemoTimeUtilities.java
+++ src/test/DemoTimeUtilities.java
@@ -1,18 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package test;
 
 import org.hapiserver.TimeUtil;
 
 /**
- *
+ * The TimeUtilities are needed to implement caching and may be useful to people
+ * using the library as well.
  * @author jbf
  */
 public class DemoTimeUtilities {
     
+    /**
+     * demo code which counts off the days in an interval.
+     */
     public static void demoCountOffDays() {
         System.err.println("\n# demoCountOffDays");
         String[] ss= TimeUtil.countOffDays("2015-12-30Z", "2016-03-02Z" );
@@ -21,17 +20,26 @@ public class DemoTimeUtilities {
         }        
     }
     
+    /**
+     * demo reformat time which will reformat arbitrary isoTime to standard format.
+     */
     public static void demoReformatTime() {
         System.err.println("\n# demoReformatTime");
         System.err.println(TimeUtil.reformatIsoTime( "2000-01-01T00:00Z", "2021-12-09T00:00:00.999888Z"));
     }
     
+    /**
+     * demo calculate the day of year function.
+     */
     public static void demoDayOfYear() {
         System.err.println("\n# demoDayOfYear");
         System.err.println(TimeUtil.dayOfYear( 2000, 12, 31 ));
         System.err.println(TimeUtil.dayOfYear( 1970, 1, 1 ));
     }
 
+    /**
+     * demo floor and ceiling functions which round down and round up to the day bounaries.
+     */
     public static void demoFloorCeil() {
         System.err.println("\n# demoFloorCeil");
         System.err.println(TimeUtil.floor("2020-05-11T08:31"));
@@ -39,7 +47,10 @@ public class DemoTimeUtilities {
         System.err.println(TimeUtil.floor("2020-05-11T00:00"));
         System.err.println(TimeUtil.ceil("2020-05-11T00:00"));
     }
-        
+     
+    /**
+     * Demo next and previous day functions, which count off days.
+     */
     public static void demoNextPrevDay() {
         System.err.println("\n# demoNextPrevDay");
         System.err.println(TimeUtil.nextDay("2020-05-11T08:31"));
@@ -49,7 +60,11 @@ public class DemoTimeUtilities {
         System.err.println(TimeUtil.previousDay("2020-05-11T00:00"));
         System.err.println(TimeUtil.previousDay("2020-12-31T00:00"));
     }
-
+    
+    /**
+     * Demo Calculate the time as long milliseconds since 1970-01-01T00:00Z, also known
+     * as "Unix Time".
+     */
     public static void demoToMillisecondsSince1970() {
         System.err.println("\n# demoToMilli");
         System.err.println(TimeUtil.toMillisecondsSince1970("2000-01-01T00:00:00.0Z"));
@@ -61,6 +76,10 @@ public class DemoTimeUtilities {
         
     }
     
+    /**
+     * Demo function for decomposing isoTime into array of 
+     * year, month, day, hour, minute, second and nanoseconds.
+     */
     public static void demoIsoTimeToArray() {
         System.err.println("\n# demoIsoToTimeArray");
         int[] arr= TimeUtil.isoTimeToArray("2001-02-03T04:05:06:000000007");
