bd07e0eb2459b14e56c54acdeededb178580b279
jbfaden <faden@cottagesystems.com>
2020-05-11 09:22:43 -0500

    demo subset in Java

---
+++ src/test/DemoReadSubset.java
@@ -0,0 +1,28 @@
+
+package test;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Iterator;
+import static org.hapiserver.HapiClient.*;
+import org.hapiserver.HapiRecord;
+import org.json.JSONException;
+
+/**
+ * Demo reading a subset of the parameters.
+ * @author jbf
+ */
+public class DemoReadSubset {
+    
+    public static void main( String[] args ) throws MalformedURLException, IOException, JSONException {
+        URL hapiServer=new URL("https://jfaden.net/HapiServerDemo/hapi/");
+        
+        Iterator<HapiRecord> it= getData( hapiServer, "Iowa+City+Conditions", "Temperature,Humidity", "2019-10-21T00:00", "2019-10-22T00:00" );
+        
+        while ( it.hasNext() ) {
+            HapiRecord rec= it.next();
+            System.out.println( String.format( "%s %9.3f %9.3f", rec.getIsoTime(0), rec.getDouble(1), rec.getDouble(2) ) );
+        }
+    }
+}
